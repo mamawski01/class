41a6536461a9562df1e024080eed412b1a38c81a
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2024-11-24 15:25:23 +0800

    k

--- b/src/routes/api/models/model/model.js
+++ b/src/routes/api/models/model/model.js
@@ -20,7 +20,7 @@ export function UserModel() {
     country: { type: String, required: [true, "Country is required."] },
     contactNumber1: {
       type: String,
-      required: [true, "Contact number 1 is required."],
+      required: [true, "Contact number at least 1 is required."],
     },
     contactNumber2: { type: String },
     contactNumber3: { type: String },
